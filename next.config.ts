2c813d334526edec803a9d11b0fa7532e11bb994
sorheil <sorheiltchokothe@gmail.com>
2025-04-26 16:10:23 +0100

    feat fix error config for pwa

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-// @ts-ignore
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  // tes options Next.js ici si tu en as
+  /* config options here */
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true
-  },
-});
+export default nextConfig;
